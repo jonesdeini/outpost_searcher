1cd3a3a7d1a66affaae5cc43a4361ce48d81865a
jonesdeini <jonesdeini@gmail.com>
2013-07-28 11:26:06 -0400

    ugly up this method with some branches so I don't forget the logic I wanted

--- auth_mail.rb
+++ auth_mail.rb
@@ -7,8 +7,13 @@ class AuthMail
 
   def self.get_two_step_auth_code
     Gmail.connect(GMAIL_USER, GMAIL_PASS) do |gmail|
-      gmail.inbox.emails(:unread, :from => "noreply@steampowered.com").each do |steam_email|
-        steam_email.message.body.scan(/<h2>\w{5}<\/h2>/).first
+      steam_emails = gmail.inbox.emails(:unread, :from => "noreply@steampowered.com")
+      if steam_emails > 1
+        #figure out which one is correct
+      elsif steam_emails == 1 # everything actually worked
+        steam_emails.first.message.body.scan(/<h2>\w{5}<\/h2>/).first
+      else # == 0
+        puts "NO STEAM EMAILS BRO" #log
       end
     end
   end
